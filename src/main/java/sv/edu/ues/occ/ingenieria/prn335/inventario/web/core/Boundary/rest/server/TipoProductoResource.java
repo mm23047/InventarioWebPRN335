3e58d6fb719d3cb97e5a2ea85e7d58d630c30856
EstuardoMMM <estuardo74972897@gmail.com>
2025-11-08 18:48:31 -0600

    Parte inicial de rest

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335/inventario/web/core/Boundary/rest/server/TipoProductoResource.java
@@ -0,0 +1,39 @@
+package sv.edu.ues.occ.ingenieria.prn335.inventario.web.core.Boundary.rest.server;
+
+import jakarta.inject.Inject;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import sv.edu.ues.occ.ingenieria.prn335.inventario.web.core.Control.TipoProductoDAO;
+
+
+@Path("tipo_producto")
+public class TipoProductoResource {
+
+    @Inject
+    TipoProductoDAO tipoProductoDAO;
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getTipoProducto(
+            @Min(0)
+            @DefaultValue("0")
+            @QueryParam("first")
+            int first,
+            @Max(100)
+            @DefaultValue("50")
+            @QueryParam("max")
+            int max) {
+        if (first >= 0 && max > 0 && max <= 100) {
+            try {
+                int total = tipoProductoDAO.count();
+                return  Response.ok(tipoProductoDAO.findRange(first,max)).header("Total-records", total).build();
+            } catch (Exception e) {
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).header("Server-exception","Cannot access db").build();
+            }
+        }
+         return Response.status(422).header("Missing-parameter", "first:" + first + ", max:" + max).build();
+    }
+}
