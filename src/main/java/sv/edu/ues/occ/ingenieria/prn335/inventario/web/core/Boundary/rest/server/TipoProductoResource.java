c33e237e413eda343be9881ab10c5819ad7808f0
zarich <rs23015@ues.edu.sv>
2025-11-09 20:51:56 -0600

    agregrando findbyId y delete

--- src/main/java/sv/edu/ues/occ/ingenieria/prn335/inventario/web/core/Boundary/rest/server/TipoProductoResource.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335/inventario/web/core/Boundary/rest/server/TipoProductoResource.java
@@ -7,6 +7,7 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import sv.edu.ues.occ.ingenieria.prn335.inventario.web.core.Control.TipoProductoDAO;
+import sv.edu.ues.occ.ingenieria.prn335.inventario.web.core.Entity.TipoProducto;
 
 
 @Path("tipo_producto")
@@ -17,7 +18,7 @@ public class TipoProductoResource {
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public Response getTipoProducto(
+    public Response findRange(
             @Min(0)
             @DefaultValue("0")
             @QueryParam("first")
@@ -36,4 +37,45 @@ public class TipoProductoResource {
         }
          return Response.status(422).header("Missing-parameter", "first:" + first + ", max:" + max).build();
     }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+
+    public Response findById(@PathParam("id") Long id) {
+        if(id != null){
+            try {
+             TipoProducto resp = tipoProductoDAO.buscarRegistroPorId(id);
+             if(resp != null){
+                 return Response.ok(resp).build();
+             }
+                return Response.status(Response.Status.NOT_FOUND).header("Not-found-id", "Record with id "+id+" not found").build();
+            } catch (Exception ex) {
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).header("Server-exception","Cannot access db").build();
+            }
+
+        }
+        return Response.status(422).header("Missing-parameter", "id").build();
+    }
+
+    @GET
+    @Path("{id}")
+
+    public Response delete(@PathParam("id") Long id){
+        if(id != null){
+            try {
+                TipoProducto resp = tipoProductoDAO.buscarRegistroPorId(id);
+                if(resp != null){
+                    tipoProductoDAO.eliminar(resp);
+                    return Response.noContent().build();
+                }
+                return Response.status(Response.Status.NOT_FOUND).header("Not-found-id", "Record with id "+id+" not found").build();
+            } catch (Exception ex) {
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).header("Server-exception","Cannot access db").build();
+            }
+
+        }
+        return Response.status(422).header("Missing-parameter", "id").build();
+    }
+
 }
